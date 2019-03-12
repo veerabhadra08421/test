09a82fb30d64329bf7486b3f52a97c7fa969fb41
veerabhadra08421 <veerabhadra08421@gmail.com>
2019-03-12 10:50:52 +0530

    New files has been added

---
+++ showname.java
@@ -0,0 +1,9 @@
+public class showname{
+
+	public static void main(String [] args){
+	  for(int i=1;i<=10;i++){
+	     System.out.println("Jenkins Job creating Sessions-CubeIPL....." +i);
+	   }
+
+	}
+}
